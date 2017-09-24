5db3b8d6f46946e3fb60382a925bb021596404ca
redface <redface0512@gmail.com>
2017-09-25 00:07:02 +0900

    heap-sort solved

---
+++ heap-sort.js
@@ -0,0 +1,43 @@
+function swap(arr, firstIndex, secondIndex) {
+  let targetArr = JSON.parse(JSON.stringify(arr));
+  let tmp = targetArr[firstIndex];
+  targetArr[firstIndex] = targetArr[secondIndex];
+  targetArr[secondIndex] = tmp;
+  return targetArr;
+}
+function heapify(arr, n, i) {
+  let largest = i; // init as root
+  const leftChild = 2 * i + 1, rightChild = 2 * i + 2;
+  let targetArr = JSON.parse(JSON.stringify(arr));
+
+  if (leftChild < n && arr[leftChild] > arr[largest]) largest = leftChild;
+  if (rightChild < n && arr[rightChild] > arr[largest]) largest = rightChild;
+
+  if (largest !== i) {
+    targetArr = swap(targetArr, i, largest); 
+    targetArr = heapify(targetArr, n, largest);
+  }
+
+  return targetArr;
+  
+}
+function heapSort(arr, n) {
+  let targetArr = JSON.parse(JSON.stringify(arr));
+  for(let i = 0; i < n; i++) targetArr = heapify(targetArr, n, i);
+  console.log('first loop', targetArr);
+
+  for(let i = n - 1; i >= 0; i--) {
+    const FIRST = 0, LAST = i;
+    console.log('------------');
+    console.log('first', targetArr[FIRST], 'last', targetArr[LAST]);
+    targetArr = swap(targetArr, FIRST, LAST);
+    console.log('after swap', targetArr);
+    targetArr = heapify(targetArr, i, 0);
+    console.log('after heapify', targetArr);
+  }
+  return targetArr;
+}
+
+let arr = [4,10,3,5,1];
+const n = 5;
+console.log('result', heapSort(arr, n));
